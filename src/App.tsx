f97f129e55ef6beee33c57564398421758376cab
charilaouc <charilaou.c@gmail.com>
2022-09-20 14:37:46 +0000

    add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Admin, Resource, ListGuesser } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
+import buildHasuraProvider from "ra-data-hasura";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
-    <Resource name="comments" list={ListGuesser} />
+    <Resource name="products" list={ListGuesser} />
+    <Resource name="users" list={ListGuesser} />
   </Admin>
 );
 
